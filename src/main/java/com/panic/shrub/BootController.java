15ff26f412fd1591e66298c968104cc7b2c68ebf
Jwayne79 <theduke64.JWM@gmail.com>
2023-11-30 23:13:39 -0600

    cb1 implemented

--- src/main/java/com/panic/shrub/BootController.java
+++ src/main/java/com/panic/shrub/BootController.java
@@ -1,9 +1,13 @@
 package com.panic.shrub;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 
-public class BootController {
+public class BootController implements Initializable{
 
     @FXML
     private void bootToPrimary() throws IOException {
@@ -11,7 +15,24 @@ public class BootController {
     }
     
     @FXML
+    private ChoiceBox<String> cb1;
+    
+    private String[] overunder = {"over","under"};
+    
+
+    public void initialize(){
+        
+        cb1.getItems().addAll(overunder);
+        
+    }
+    
+    @FXML
     private void bootToBoot() throws IOException {
         App.setRoot("boot");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        cb1.getItems().addAll(overunder);
     }
 }
