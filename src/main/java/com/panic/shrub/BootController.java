da23063e2d49e27e838115ce8f2aa9c4e0110428
Jwayne79 <theduke64.JWM@gmail.com>
2023-12-01 00:04:57 -0600

    cb2 implemented

--- src/main/java/com/panic/shrub/BootController.java
+++ src/main/java/com/panic/shrub/BootController.java
@@ -10,7 +10,7 @@ import javafx.scene.control.ChoiceBox;
 public class BootController implements Initializable{
 
 
-    
+    @FXML
     private void bootToPrimary() throws IOException {
         App.setRoot("primary");
     }
@@ -18,12 +18,17 @@ public class BootController implements Initializable{
     @FXML
     private ChoiceBox<String> cb1;
     
+    @FXML
+    private ChoiceBox<String> cb2;
+    
     private String[] overunder = {"over","under"};
+    private String[] units = {"calories","g of fat","g of protein","g of fiber","g of sugar","hours", "oz of water"};
     
 
     public void initialize(){
         
         cb1.getItems().addAll(overunder);
+        cb2.getItems().addAll(units);
         
     }
     
@@ -35,6 +40,7 @@ public class BootController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         cb1.getItems().addAll(overunder);
+        cb2.getItems().addAll(units);
        
     }
 }
